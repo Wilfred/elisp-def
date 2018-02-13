87972d7867c16bebe89ef02ac066379cfd1286d3
Wilfred Hughes <me@wilfred.me.uk>
2018-02-13 19:10:58 +0000

    First draft go-to-definition

---
+++ elisp-def.el
@@ -0,0 +1,120 @@
+;;; elisp-def.el --- macro-aware go-to-definition for elisp  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Wilfred Hughes
+
+;; Author: Wilfred Hughes <me@wilfred.me.uk>
+;; Keywords: lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; TODO
+
+;;; Code:
+
+(require 'dash)
+(require 'f)
+(require 'find-func)
+
+(defun elisp-def--find-library-name (path)
+  "A wrapper around `find-library-name' that returns nil if PATH
+has no library with that name.
+
+This can happen when users have installed Emacs without its
+source code: they have e.g. org.elc but no org.el."
+  (condition-case _err
+      (find-library-name path)
+    (error nil)))
+
+(defun elisp-def--primitive-p (sym callable-p)
+  "Return t if SYM is defined in C."
+  (if callable-p
+      (subrp (indirect-function sym))
+    (let ((filename (find-lisp-object-file-name sym 'defvar)))
+      (or (eq filename 'C-source)
+          (and (stringp filename)
+               (equal (file-name-extension filename) "c"))))))
+
+(defun elisp-def--find-global (sym callable-p)
+  "Find the buffer and position where SYM is globally defined."
+  (let ((primitive-p (elisp-def--primitive-p sym callable-p))
+        (path nil)
+        (buf nil)
+        (pos nil))
+    (when callable-p
+      (-let [(base-sym . src-path) (find-function-library sym)]
+        ;; `base-sym' is the underlying symbol if `sym' is an alias.
+        (setq sym base-sym)
+        (setq path src-path)))
+    (when (and primitive-p path find-function-C-source-directory)
+      ;; Convert "src/foo.c" to "".
+      (setq path (f-expand path
+                           (f-parent find-function-C-source-directory))))
+
+    (cond
+     ((and callable-p path)
+      ;; Convert foo.elc to foo.el.
+      (-when-let (src-path (elisp-def--find-library-name path))
+        ;; Open `path' ourselves, so we can widen before searching.
+        (setq buf (find-file-noselect src-path))
+
+        ;; Based on `find-function-noselect'.
+        (with-current-buffer buf
+          ;; If the symbol is defined outside the current range, widen, otherwise preserve narrowing.
+          (save-restriction
+            (widen)
+            (setq pos
+                  (if primitive-p
+                      (cdr (find-function-C-source sym path nil))
+                    (cdr (find-function-search-for-symbol sym nil path)))))
+          (when (or (< pos (point-min))
+                    (> pos (point-max)))
+            (widen)))))
+     (callable-p
+      ;; Functions defined interactively may have an edebug property
+      ;; that contains the location of the definition.
+      (-when-let (edebug-info (get sym 'edebug))
+        (-let [marker (if (consp edebug-info)
+                          (car edebug-info)
+                        edebug-info)]
+          (setq buf (marker-buffer marker))
+          (setq pos (marker-position marker)))))
+     ((not callable-p)
+      (condition-case _err
+          (-let [(sym-buf . sym-pos) (find-definition-noselect sym 'defvar)]
+            (setq buf sym-buf)
+            (setq pos sym-pos))
+        (search-failed nil)
+        ;; If your current Emacs instance doesn't match the source
+        ;; code configured in find-function-C-source-directory, we can
+        ;; get an error about not finding source. Try
+        ;; `default-tab-width' against Emacs trunk.
+        (error nil))))
+    (list buf pos)))
+
+(defun elisp-def ()
+  "Go to the definition of the symbol at point."
+  (interactive)
+  (-let* ((sym (symbol-at-point))
+          ((buf pos) (elisp-def--find-global sym t)))
+    (switch-to-buffer buf)
+    ;; TODO: It would be nice to flash the symbol name too.
+    (goto-char pos)))
+
+;; Overriding xref-find-definitions.
+(global-set-key (kbd "M-.") #'elisp-def)
+
+(provide 'elisp-def)
+;;; elisp-def.el ends here
