a136373f0ee92bb8d4a5cf3aaf079ef513a625d4
Wilfred Hughes <me@wilfred.me.uk>
2018-02-18 11:41:39 +0000

    Expand commentary

--- elisp-def.el
+++ elisp-def.el
@@ -21,7 +21,11 @@
 
 ;;; Commentary:
 
-;; Find the definition of the symbol at point, intelligently.
+;; Find the definition of the symbol at point,
+;; intelligently. Understands namespaces, macros, libraries and local
+;; bindings.
+;;
+;; See full docs at https://github.com/Wilfred/elisp-def
 ;;
 ;; TODO: fonts
 
