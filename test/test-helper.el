5f769317154c4437463e11393beca4aadb979ed2
Wilfred Hughes <me@wilfred.me.uk>
2018-02-24 15:42:36 +0000

    Configuring undercover

--- test/test-helper.el
+++ test/test-helper.el
@@ -6,4 +6,9 @@
 (let ((elisp-def-dir (f-parent (f-dirname (f-this-file)))))
   (add-to-list 'load-path elisp-def-dir))
 
+(require 'undercover)
+(undercover "elisp-def.el"
+	    (:exclude "*-test.el")
+	    (:report-file "/tmp/undercover-report.json"))
+
 ;;; test-helper.el ends here
