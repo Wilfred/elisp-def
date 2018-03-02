5e0d766a5db537f4e7145c2053c22829d736ceba
Wilfred Hughes <me@wilfred.me.uk>
2018-03-02 21:29:37 +0000

    Call this version 1.0

--- elisp-def.el
+++ elisp-def.el
@@ -1,7 +1,7 @@
 ;;; elisp-def.el --- macro-aware go-to-definition for elisp  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2018  Wilfred Hughes
-;; Version: 0.1
+;; Version: 1.0
 
 ;; Author: Wilfred Hughes <me@wilfred.me.uk>
 ;; Keywords: lisp
