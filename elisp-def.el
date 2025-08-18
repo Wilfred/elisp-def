61a5f64498c9c8de8e9aab84a22775162f336144
Wilfred Hughes <me@wilfred.me.uk>
2025-08-18 23:23:11 +0100

    Roll version

--- elisp-def.el
+++ elisp-def.el
@@ -1,7 +1,7 @@
 ;;; elisp-def.el --- macro-aware go-to-definition for elisp  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2020  Wilfred Hughes
-;; Version: 1.2
+;; Copyright (C) 2020-2025  Wilfred Hughes
+;; Version: 1.3
 
 ;; Author: Wilfred Hughes <me@wilfred.me.uk>
 ;; Keywords: lisp
